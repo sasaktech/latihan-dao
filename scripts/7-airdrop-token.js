f3852b77fe3f19da29bd5451c7b03caac48af50b
Dian <dian@petanikode.com>
2022-08-11 00:40:26 +0800

    add airdop script, but not working yet

---
+++ scripts/7-airdrop-token.js
@@ -0,0 +1,43 @@
+import sdk from "./1-initialize-sdk.js";
+
+// ERC-1155 contract address
+const editionDrop = sdk.getEditionDrop("0x6Ea01b5928c12Af82D7525F4f2D3713d8Ab49495");
+// ERC-20 address
+const token = sdk.getToken("0xE9f6d55371d574Ed77778F4a13985b664E4388B1");
+
+(async () => {
+  try {
+    // Grab all the addresses of people who own our membership NFT, 
+    // which has a tokenId of 0.
+    const walletAddresses = await editionDrop.history.getAllClaimerAddresses(0);
+
+    if (walletAddresses.length === 0) {
+      console.log(
+        "No NFTs have been claimed yet, maybe get some friends to claim your free NFTs!",
+      );
+      process.exit(0);
+    }
+
+    // Loop through the array of addresses.
+    const airdropTargets = walletAddresses.map((address) => {
+      // Pick a random # between 1000 and 10000.
+      const randomAmount = Math.floor(Math.random() * (10000 - 1000 + 1) + 1000);
+      console.log("✅ Going to airdrop", randomAmount, "tokens to", address);
+
+      // Set up the target.
+      const airdropTarget = {
+        toAddress: address,
+        amount: randomAmount,
+      };
+
+      return airdropTarget;
+    });
+
+    // Call transferBatch on all our airdrop targets.
+    console.log("🌈 Starting airdrop...");
+    await token.transferBatch(airdropTargets);
+    console.log("✅ Successfully airdropped tokens to all the holders of the NFT!");
+  } catch (err) {
+    console.error("Failed to airdrop tokens", err);
+  }
+})();
